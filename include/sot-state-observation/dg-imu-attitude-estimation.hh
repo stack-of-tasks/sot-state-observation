3ee13ad83d09053ab962fe9c0d62a8dd34d51779
Mehdi Benallegue <mehdi.benallegue@gmail.com>
2013-11-27 22:22:28 +0100

    Add the flexibility estimator implementation in the dynamic graph

---
+++ include/sot-state-observation/dg-imu-attitude-estimation.hh
@@ -0,0 +1,122 @@
+/*
+ *  Copyright 2013 CNRS
+ *
+ *  Mehdi Benallegue
+ */
+
+#ifndef SOT_DYNAMIC_GRAPH_IMU_ATTITTUDE_ESTIMATION_HH
+#define SOT_DYNAMIC_GRAPH_IMU_ATTITTUDE_ESTIMATION_HH
+
+#include <dynamic-graph/entity.h>
+#include <dynamic-graph/signal-ptr.h>
+#include <dynamic-graph/signal-time-dependent.h>
+#include <dynamic-graph/linear-algebra.h>
+
+#include <state-observation/dynamical-system/imu-dynamical-system.hpp>
+#include <state-observation/dynamical-system/dynamical-system-simulator.hpp>
+#include <state-observation/observer/extended-kalman-filter.hpp>
+#include <state-observation/tools/miscellaneous-algorithms.hpp>
+
+#include <sot-state-observation/tools/definitions.hh>
+
+namespace sotStateObservation
+{
+        /**
+           \brief
+        */
+        class DynamicGraphIMUAttitudeEstimation :
+            public dynamicgraph::Entity,
+            private boost::noncopyable //
+        {
+        public:
+            /**
+            \brief Constructor by name
+            */
+            DynamicGraphIMUAttitudeEstimation(const std::string& inName);
+
+            ~DynamicGraphIMUAttitudeEstimation();
+
+            /// Each entity should provide the name of the class it belongs to
+            virtual const std::string& getClassName (void) const
+            {
+                return CLASS_NAME;
+            }
+
+            /// Header documentation of the python class
+            virtual std::string getDocString () const
+            {
+                return
+                    "A state observer which takes an IMU and gives the attitude\n";
+            }
+
+            void setStateGuess (const ::dynamicgraph::Vector & xh0)
+            {
+                filter_.setState(convertVector<stateObservation::Vector>(xh0),attitudeSOUT.getTime());
+            }
+
+            void setStateGuessCovariance (const ::dynamicgraph::Matrix & p)
+            {
+                filter_.setStateCovariance(convertMatrix<stateObservation::Matrix>(p));
+            }
+
+            void setSensorsNoiseCovariance (const ::dynamicgraph::Matrix & r)
+            {
+                filter_.setR(convertMatrix<stateObservation::Matrix>(r));
+            }
+
+            void setProcessNoiseCovariance (const ::dynamicgraph::Matrix & q)
+            {
+                filter_.setQ(convertMatrix<stateObservation::Matrix>(q));
+            }
+
+
+
+            /**
+            \name Parameters
+            @{
+            */
+        protected:
+            /*
+            \brief Class name
+            */
+            static const std::string CLASS_NAME;
+
+        private:
+            /**
+            Compute the control law
+            */
+            ::dynamicgraph::Vector& computeAttitude
+                        (::dynamicgraph::Vector & Attitude, const int& inTime);
+
+            /**
+            \brief Measurement of the IMU
+            */
+            dynamicgraph::SignalPtr < ::dynamicgraph::Vector, int> measurementSIN;
+
+            /**
+            \brief Input of the dynamical system
+            */
+            dynamicgraph::SignalPtr < ::dynamicgraph::Vector, int> inputSIN;
+
+            /**
+            \brief Estimation of the attitude
+            */
+            dynamicgraph::SignalTimeDependent < ::dynamicgraph::Vector, int> attitudeSOUT;
+
+            ///initialization of the extended Kalman filter
+            stateObservation::ExtendedKalmanFilter filter_;
+
+            ///initalization of the functor
+            stateObservation::IMUDynamicalSystem imuFunctor_;
+
+            ///Sizes of the states for the state, the measurement, and the input vector
+            static const unsigned stateSize=18;
+            static const unsigned measurementSize=6;
+            static const unsigned inputSize=6;
+
+        };
+
+} // namespace sotStateObservation
+
+#endif //SOT_DYNAMIC_GRAPH_IMU_ATTITTUDE_ESTIMATION_HH
+
---
+++ src/dg-imu-attitude-estimation.cc
@@ -0,0 +1,146 @@
+#include <sstream>
+
+#include <dynamic-graph/factory.h>
+#include <dynamic-graph/command-setter.h>
+#include <dynamic-graph/command-getter.h>
+#include <dynamic-graph/command-bind.h>
+
+#include <sot-state-observation/dg-imu-attitude-estimation.hh>
+
+
+
+namespace sotStateObservation
+{
+    DYNAMICGRAPH_FACTORY_ENTITY_PLUGIN ( DynamicGraphIMUAttitudeEstimation,
+                                          "DGIMUAttitudeEstimation" );
+
+    DynamicGraphIMUAttitudeEstimation::DynamicGraphIMUAttitudeEstimation
+                ( const std::string & inName):
+        Entity(inName),
+        measurementSIN(0x0 , "DGIMUAttitudeEstimation("+inName+")::input(vector)::measurement"),
+        inputSIN(0x0 , "DGIMUAttitudeEstimation("+inName+")::input(vector)::input"),
+        attitudeSOUT(measurementSIN,
+                        "DGIMUAttitudeEstimation("+inName+")::input(vector)::attitude"),
+        filter_(stateSize, measurementSize, inputSize, false)
+    {
+        signalRegistration (measurementSIN);
+        signalRegistration (inputSIN);
+        signalRegistration (attitudeSOUT);
+
+        dynamicgraph::Vector measure(measurementSize);
+        dynamicgraph::Vector input(inputSize);
+        dynamicgraph::Vector attitude(stateSize);
+
+        measurementSIN.setConstant(measure);
+        inputSIN.setConstant(input);
+        attitudeSOUT.setConstant(attitude);
+
+        attitudeSOUT.setFunction(boost::bind(&DynamicGraphIMUAttitudeEstimation::computeAttitude,
+				    this, _1, _2));
+
+        std::ostringstream stateSizeString;
+        stateSizeString << stateSize;
+
+        std::ostringstream measurementSizeString;
+        stateSizeString << measurementSize;
+
+        std::ostringstream inputSizeString;
+        inputSizeString << inputSize;
+
+        std::string docstring;
+
+        //setStateGuess
+        docstring =
+                "\n"
+                "    Set a guess of the state  \n"
+                "    takes a tuple of " + stateSizeString.str() + "\n"
+                "    floating point mumbers as input \n"
+                "\n";
+
+        addCommand(std::string("setStateGuess"),
+	     new
+	     ::dynamicgraph::command::Setter <DynamicGraphIMUAttitudeEstimation,dynamicgraph::Vector>
+	     (*this, &DynamicGraphIMUAttitudeEstimation::setStateGuess, docstring));
+
+
+         //setStateGuessCovariance
+        docstring =
+                "\n"
+                "    Set the covariance matrix of the current state estimation \n"
+                "    takes " + stateSizeString.str() + " tuples of" + stateSizeString.str() + "\n"
+                "    floating point mumbers as input \n"
+                "\n";
+
+        addCommand(std::string("setStateGuessCovariance"),
+	     new
+	     ::dynamicgraph::command::Setter <DynamicGraphIMUAttitudeEstimation,dynamicgraph::Matrix>
+	     (*this, &DynamicGraphIMUAttitudeEstimation::setStateGuessCovariance, docstring));
+
+
+        //setSensorsNoiseCovariance
+        docstring =
+                "\n"
+                "    Set the covariance matrix of the sensor noise \n"
+                "    takes " + measurementSizeString.str() + "tuples of" + measurementSizeString.str() + "\n"
+                "    floating point mumbers as input \n"
+                "\n";
+
+        addCommand(std::string("setSensorsNoiseCovariance"),
+	     new
+	     ::dynamicgraph::command::Setter <DynamicGraphIMUAttitudeEstimation,dynamicgraph::Matrix>
+	     (*this, &DynamicGraphIMUAttitudeEstimation::setSensorsNoiseCovariance, docstring));
+
+
+        //setProcessNoiseCovariance
+        docstring =
+                "\n"
+                "    Set the covariance matrix of the process noise \n"
+                "    takes " + stateSizeString.str() + " tuples of" + stateSizeString.str() + "\n"
+                "    floating point mumbers as input \n"
+                "\n";
+
+        addCommand(std::string("setProcessNoiseCovariance"),
+	     new
+	     ::dynamicgraph::command::Setter <DynamicGraphIMUAttitudeEstimation,dynamicgraph::Matrix>
+	     (*this, &DynamicGraphIMUAttitudeEstimation::setProcessNoiseCovariance, docstring));
+
+
+        filter_.setState(stateObservation::Vector::Zero(stateSize), attitudeSOUT.getTime());
+
+        filter_.setFunctor(&imuFunctor_);
+
+        ///The covariance matrix of the process noise and the measurement noise
+        stateObservation::Matrix q = stateObservation::Matrix::Identity(stateSize,stateSize)*0.01;
+        stateObservation::Matrix r = stateObservation::Matrix::Identity(measurementSize,measurementSize)*100;
+
+        filter_.setQ(q);
+        filter_.setR(r);
+
+        stateObservation::Vector xh0=stateObservation::Vector::Zero(stateSize,1);
+
+        stateObservation::Matrix p=stateObservation::Matrix::Identity(stateSize,stateSize)*0.1;
+
+        filter_.setStateCovariance(p);
+
+        filter_.setState(xh0,attitudeSOUT.getTime());
+
+    }
+
+    DynamicGraphIMUAttitudeEstimation::~DynamicGraphIMUAttitudeEstimation()
+    {
+    }
+
+    dynamicgraph::Vector& DynamicGraphIMUAttitudeEstimation::computeAttitude
+                  (dynamicgraph::Vector & Attitude, const int& inTime)
+    {
+        const dynamicgraph::Vector & measurement = measurementSIN(inTime);
+        const dynamicgraph::Vector & input = inputSIN(inTime-1);
+
+        filter_.setMeasurement(convertVector<stateObservation::Vector>(measurement),inTime);
+        filter_.setInput(convertVector<stateObservation::Vector>(input),inTime-1);
+
+        Attitude = convertVector<dynamicgraph::Vector>(filter_.getEstimateState(inTime));
+
+        return Attitude;
+    }
+}
