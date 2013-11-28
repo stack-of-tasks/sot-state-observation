cbfd6de40b5ce81e5b16487af28d2d796e3298b9
Mehdi Benallegue <mehdi.benallegue@gmail.com>
2013-11-28 18:39:25 +0100

    A test is introduced to check the instanciation of the classes

---
+++ unit-testing/instanciation-test.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+
+#include <sot-state-observation/dg-imu-attitude-estimation.hh>
+#include <sot-state-observation/dg-imu-flexibility-estimation.hh>
+
+
+using namespace sotStateObservation;
+
+struct instanciator
+{
+    instanciator():
+        f("Hey"),
+        a("Ho")
+    {
+        std::cout << "Instanciation succeeded" << std::endl;
+
+
+    }
+
+    DGIMUFlexibilityEstimation f;
+    DGIMUAttitudeEstimation a;
+};
+
+int main()
+{
+    instanciator i;
+    return 0;
+
+}
