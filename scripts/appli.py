a1bd49f3298128918da757fba691478cd0316de5
Mehdi Benallegue <mehdi.benallegue@gmail.com>
2013-12-09 22:52:41 +0100

    update the scenario to use the flexibility estimator

--- scripts/appli.py
+++ scripts/appli.py
@@ -1,4 +1,12 @@
 # Launch it with py ../robotViewerLauncher.py +compensater.py +appli.py 
+import sys
+import numpy as np
+import matplotlib.pyplot as pl
+import dynamic_graph as dg
+import dynamic_graph.signal_base as dgsb
+import dynamic_graph.sot.core.sot_state_observation as sotso
+from dynamic_graph.sot.application.compensater import *
+from math import sin
 
 appli = CompensaterApplication(robot)
 appli.withTraces()
@@ -27,5 +35,27 @@ t = optionalparentheses(appli.trace)
 a = appli
 s = optionalparentheses(appli.nextStep)
 
-#stop()
+#### Flexibility Estimator ##
+a= sotso.DGIMUFlexibilityEstimation('flextimator')
+a.setSamplingPeriod(0.005)
 
+meas = a.signal('measurement')
+inputs = a.signal('input')
+contactNbr = a.signal('contactNbr')
+contactNbr.value = 1
+
+contact1 = a.signal('contact1')
+contact1.value = (0,0,0);
+
+flex=a.signal('flexPoseThetaU')
+
+
+plug(flex,appli.ccMc.sin)
+
+meas.value = (0.0 ,  9.8 , 0.0 , 0.0 , 0.0 , 0.0 )
+inputs.value = (0.0, 0.0, 1.8, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
+
+
+
+
+#stop()
