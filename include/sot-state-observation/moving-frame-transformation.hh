0f238d706c0e6cb3a9ff7791d612046c9874ed00
Mehdi Benallegue <mehdi.benallegue@gmail.com>
2014-01-07 20:34:40 +0100

    Add the moving frame transformer entity.

---
+++ include/sot-state-observation/moving-frame-transformation.hh
@@ -0,0 +1,108 @@
+/*
+ *  Copyright 2013 CNRS
+ *
+ *  Mehdi Benallegue
+ */
+
+#ifndef MOVING_FRAME_TRANSFORMATION_HH
+#define MOVING_FRAME_TRANSFORMATION_HH
+
+#include <dynamic-graph/entity.h>
+#include <dynamic-graph/signal-ptr.h>
+#include <dynamic-graph/signal-time-dependent.h>
+#include <dynamic-graph/linear-algebra.h>
+#include <sot/core/matrix-homogeneous.hh>
+
+#include <sot-state-observation/tools/definitions.hh>
+
+
+#include <state-observation/tools/miscellaneous-algorithms.hpp>
+
+
+namespace sotStateObservation
+{
+        /**
+           \brief
+        */
+        class MovingFrameTransformation :
+            public dynamicgraph::Entity
+        {
+        public:
+            /**
+            \brief Constructor by name
+            */
+            MovingFrameTransformation(const std::string& inName);
+
+            ~MovingFrameTransformation();
+
+            /// Each entity should provide the name of the class it belongs to
+            virtual const std::string& getClassName (void) const
+            {
+                return CLASS_NAME;
+            }
+
+            /// Header documentation of the python class
+            virtual std::string getDocString () const
+            {
+                return
+                    std::string("Transform cordinates (lM0) and velocities (lV0)")
+                              + "in a moving local frame into global frame."
+                              + "The local frame is at cordinate gMl and velocity gVl";
+            }
+
+            /**
+            \name Parameters
+            @{
+            */
+        protected:
+            /*
+            \brief Class name
+            */
+            static const std::string CLASS_NAME;
+
+        private:
+            /**
+            Compute the control law
+            */
+            ::dynamicgraph::sot::MatrixHomogeneous& computegM0
+                        (::dynamicgraph::sot::MatrixHomogeneous & computegM0, const int& inTime);
+
+            ::dynamicgraph::Vector& computegV0
+                        (::dynamicgraph::Vector & velocity, const int& intTime);
+
+            /**
+            \brief local to global frame position
+            */
+            dynamicgraph::SignalPtr < ::dynamicgraph::sot::MatrixHomogeneous, int> gMlSIN;
+
+            /**
+            \brief local to global velocity vector
+            */
+            dynamicgraph::SignalPtr < ::dynamicgraph::Vector, int> gVlSIN;
+
+            /**
+            \brief Homogeneous matrix in the local frame
+            */
+            dynamicgraph::SignalPtr < ::dynamicgraph::sot::MatrixHomogeneous, int> lM0SIN;
+
+            /**
+            \brief velocity vector in the local frame
+            */
+            dynamicgraph::SignalPtr < ::dynamicgraph::Vector, int> lV0SIN;
+
+
+            /**
+            \brief Estimation of the attitude
+            */
+            dynamicgraph::SignalTimeDependent < ::dynamicgraph::sot::MatrixHomogeneous, int> gM0SOUT;
+
+            /**
+            \brief Estimation of the attitude
+            */
+            dynamicgraph::SignalTimeDependent < ::dynamicgraph::Vector, int> gV0SOUT;
+        };
+
+} // namespace sotStateObservation
+
+#endif //MOVING_FRAME_TRANSFORMATION_HH
+
---
+++ src/moving-frame-transformation.cc
@@ -0,0 +1,108 @@
+#include <sstream>
+
+#include <dynamic-graph/factory.h>
+#include <jrl/mal/matrixabstractlayervector3jrlmath.hh>
+#include <sot-state-observation/moving-frame-transformation.hh>
+
+
+namespace sotStateObservation
+{
+    DYNAMICGRAPH_FACTORY_ENTITY_PLUGIN ( MovingFrameTransformation,
+                                "MovingFrameTransformation" );
+
+    MovingFrameTransformation::MovingFrameTransformation
+                                        ( const std::string & inName):
+        Entity(inName),
+        gMlSIN(0x0 , "MovingFrameTransformation("+inName+")::input(MatrixHomogeneous)::gMl"),
+        gVlSIN(0x0 , "MovingFrameTransformation("+inName+")::input(vector)::gVl"),
+        lM0SIN(0x0 , "MovingFrameTransformation("+inName+")::input(MatrixHomogeneous)::lM0"),
+        lV0SIN(0x0 , "MovingFrameTransformation("+inName+")::input(vector)::lV0"),
+        gM0SOUT( gMlSIN<<lM0SIN,
+                        "DGIMUAttitudeEstimation("+inName+")::output(vector)::gM0"),
+        gV0SOUT( gMlSIN<<gVlSIN<<lM0SIN<<lV0SIN,
+                        "DGIMUAttitudeEstimation("+inName+")::output(vector)::gV0")
+    {
+        signalRegistration (gMlSIN);
+        signalRegistration (gVlSIN);
+        signalRegistration (lM0SIN);
+        signalRegistration (lV0SIN);
+        signalRegistration (gM0SOUT);
+        signalRegistration (gV0SOUT);
+
+        dynamicgraph::Vector velocity (6);
+        dynamicgraph::Matrix homoMatrix(4,4);
+
+        homoMatrix.setIdentity();
+
+        gMlSIN.setConstant(homoMatrix);
+        gVlSIN.setConstant(velocity);
+        lM0SIN.setConstant(homoMatrix);
+        lV0SIN.setConstant(velocity);
+        gM0SOUT.setConstant(homoMatrix);
+        gV0SOUT.setConstant(velocity);
+
+
+        gM0SOUT.setFunction(boost::bind(&MovingFrameTransformation::computegM0,
+				    this, _1, _2));
+
+        gV0SOUT.setFunction(boost::bind(&MovingFrameTransformation::computegV0,
+				    this, _1, _2));
+
+
+
+    }
+
+    MovingFrameTransformation::~MovingFrameTransformation()
+    {
+    }
+
+    dynamicgraph::Vector& MovingFrameTransformation::computegV0
+                (dynamicgraph::Vector & velocity, const int& inTime)
+    {
+        dynamicgraph::sot::MatrixHomogeneous gMl (gMlSIN(inTime));
+        dynamicgraph::sot::MatrixHomogeneous lM0 (lM0SIN(inTime));
+        dynamicgraph::Vector gVl (gVlSIN(inTime));
+        dynamicgraph::Vector lV0 (lV0SIN(inTime));
+
+        dynamicgraph::Matrix gRl(3,3);
+
+        dynamicgraph::Vector lT0(3);
+
+        dynamicgraph::Vector gOmegal(3);
+
+        gMl.extract(gRl);
+
+        lM0.extract(lT0);
+
+        gOmegal = getSubvector(gVl,3,3);
+
+        dynamicgraph::Vector omega = gRl * getSubvector(lV0,3,3) + gOmegal;
+
+
+        dynamicgraph::Vector tdot = crossProduct(gOmegal , gRl * lT0)
+                                    + gRl * getSubvector(lV0,0,3)
+                                        + getSubvector(gVl,0,3);
+
+        velocity.resize(6);
+        setSubvector(velocity, 0, tdot );
+        setSubvector(velocity, 3, omega);
+
+        std::cout << "computegV0" << std::endl;
+
+        return velocity;
+
+    }
+
+    ::dynamicgraph::sot::MatrixHomogeneous& MovingFrameTransformation::computegM0
+                (::dynamicgraph::sot::MatrixHomogeneous & homo, const int& inTime)
+    {
+        ::dynamicgraph::sot::MatrixHomogeneous gMl = gMlSIN(inTime);
+        ::dynamicgraph::sot::MatrixHomogeneous lM0 = lM0SIN(inTime);
+
+        homo = gMl * lM0;
+
+        //std::cout << "computegM0" << std::endl;
+
+        return homo;
+    }
+}
