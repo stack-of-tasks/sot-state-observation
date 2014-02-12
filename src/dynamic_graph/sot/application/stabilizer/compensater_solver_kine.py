b38ccfed7a353aa1951c84674687942da884f890
Mehdi Benallegue <mehdi.benallegue@gmail.com>
2014-02-12 20:45:55 +0100

    Change the solver of the compensater to the classical one and keep the solver kine version

--- src/dynamic_graph/sot/application/stabilizer/compensater_solver_kine.py
+++ src/dynamic_graph/sot/application/stabilizer/compensater_solver_kine.py
@@ -1,84 +1,119 @@
 from dynamic_graph.sot.core import *
 from dynamic_graph.sot.core.meta_task_6d import MetaTask6d,toFlags
 from dynamic_graph.sot.core.meta_task_visual_point import MetaTaskVisualPoint
-from dynamic_graph.sot.core.meta_tasks import *
+from dynamic_graph.sot.core.meta_tasks_kine import *
 from numpy import *
 from numpy.linalg import inv,pinv,norm
 from dynamic_graph.sot.core.matrix_util import matrixToTuple, vectorToTuple,rotate,matrixToRPY
 import dynamic_graph.sot.core.matrix_util 
-rpyToMatrix = RPYToMatrix
-#from dynamic_graph.sot.core.meta_tasks_relative import gotoNdRel, MetaTask6dRel
-from dynamic_graph.sot.core.meta_task_posture import MetaTaskPosture
+rpyToMatrix = matrix_util.RPYToMatrix
+from dynamic_graph.sot.core.meta_tasks_kine_relative import gotoNdRel, MetaTaskKine6dRel
+from dynamic_graph.sot.core.meta_task_posture import MetaTaskKinePosture
 from dynamic_graph.sot.core.feature_vector3 import FeatureVector3
+from dynamic_graph.sot.dyninv import SolverKine
 from dynamic_graph.tracer_real_time import *
 
 import dynamic_graph.sot.application.sot_state_observation as sotso
-from dynamic_graph.sot.application.velocity.precomputed_tasks import Application
+from dynamic_graph.sot.application.velocity.precomputed_meta_tasks import initialize
 
-toList = lambda sot: map(lambda x: x[2:],sot.display().split('\n')[3:-2])
+SolverKine.toList = lambda sot: map(lambda x: x[1:-1],sot.dispStack().split('|')[1:])
 
-def change6dPositionReference(task,feature,gain,position,selec=None,ingain=None,resetJacobian=True):
-    M=generic6dReference(position)
-    if selec!=None:
-        if isinstance(selec,str):  feature.selec.value = selec
-        else: feature.selec.value = toFlags(selec)
-    feature.reference.value = matrixToTuple(M)
-    if gain!=None:  setGain(gain,ingain)
-    if 'resetJacobianDerivative' in task.__class__.__dict__.keys() and resetJacobian:
-        task.resetJacobianDerivative()
 
-class HandCompensater(Application):
+def rtToHomo(R,T=None):
+    M=eye(4)
+    M[0:3,0:3]=R
+    if T!=None: M[0:3,3]=T
+    return matrix(M)
 
+class HandCompensaterKine:
+    
     threePhaseScrew = True
     tracesRealTime = True
 
-    def __init__(self,robot,twoHands = True):
-        Application.__init__(self,robot)
-
+    def __init__(self,robot,twoHands=True):
+        self.robot = robot
         self.twoHands = twoHands
 
-        self.robot = robot
         plug(self.robot.dynamic.com,self.robot.device.zmp)
-        
+
+        self.initGeneralApplication()
         self.sot = self.solver.sot
 
         self.createTasks()
         self.initTasks()
         self.initTaskGains()
 
+        self.initSolver()
         self.initialStack()
+        
+        
+
+
+    def initGeneralApplication(self):
+        self.solver = initialize(self.robot)
+
 
     # --- TASKS --------------------------------------------------------------------
     # --- TASKS --------------------------------------------------------------------
     # --- TASKS --------------------------------------------------------------------
     def createTasks(self):
 
-        self.taskbalance = self.tasks['balance']
-        self.taskRH      = self.tasks['right-wrist']
-        self.taskLH      = self.tasks['left-wrist']
-        self.taskChest   = self.tasks['chest']
-        self.taskPosture = self.tasks['posture']
-        self.taskHalfStitting = MetaTaskPosture(self.robot.dynamic,'halfsitting')
+        self.taskGaze    = MetaTaskVisualPoint('gaze',self.robot.dynamic,'head','gaze')
+        self.contactRF   = self.robot.contactRF
+        self.contactLF   = self.robot.contactLF
+        self.taskCom     = self.robot.mTasks['com']
+        self.taskLim     = self.robot.taskLim
+        self.taskRH      = self.robot.mTasks['rh']
+        if self.twoHands:
+            self.taskLH      = self.robot.mTasks['lh']
+        self.taskChest   = self.robot.mTasks['chest']
+        self.taskPosture = self.robot.mTasks['posture']
+        self.taskGripper = MetaTaskKinePosture(self.robot.dynamic,'gripper')
+        self.taskHalfStitting = MetaTaskKinePosture(self.robot.dynamic,'halfsitting')
+        self.taskCompensateR = MetaTaskKine6d('compensateR',self.robot.dynamic,'rh','right-wrist')
+        if self.twoHands:
+            self.taskCompensateL = MetaTaskKine6d('compensateL',self.robot.dynamic,'lh','left-wrist')
+
+    def openGripper(self):
+        self.taskGripper.gotoq(None,rhand=(self.gripperOpen,),lhand=(self.gripperOpen,))
+
+    def closeGripper(self):
+        self.taskGripper.gotoq(None,rhand=(self.gripperClose,),lhand=(self.gripperClose,))
 
     #initialization is separated from the creation of the tasks because if we want to switch
     #to second order controlm the initialization will remain while the creation is 
     #changed
 
     def initTasks(self):
+        self.initTaskContact()
         self.initTaskBalance()
         self.initTaskPosture()
-        #self.initTaskHalfSitting()
+        self.initTaskHalfSitting()
         self.initTaskCompensate()
 
-    #def initTaskHalfSitting(self):
-    #    self.taskHalfStitting.gotoq(None,self.robot.halfSitting)
+    def initTaskHalfSitting(self):
+        self.taskHalfStitting.gotoq(None,self.robot.halfSitting)
+
+    def initTaskContact(self):
+        # --- CONTACTS
+        self.contactRF.feature.frame('desired')
+        self.contactLF.feature.frame('desired')
 
     def initTaskBalance(self):
         # --- BALANCE ---
-        self.features['chest'].frame('desired')
+        self.taskChest.feature.frame('desired')
         #self.taskChest.feature.selec.value = '111111'
-        self.features['chest'].selec.value = '111100'
-        self.featureCom.selec.value = '011'
+        self.taskChest.feature.selec.value = '111000'
+
+
+        #self.taskCom.feature.selec.value = '111'
+        
+        ljl = matrix(self.robot.dynamic.lowerJl.value).T
+        ujl = matrix(self.robot.dynamic.upperJl.value).T
+        ljl[9] = 0.65
+        ljl[15] = 0.65
+        self.taskLim.referenceInf.value = vectorToTuple(ljl)
+        self.taskLim.referenceSup.value = vectorToTuple(ujl)
 
     def initTaskPosture(self):
         # --- LEAST NORM
@@ -94,41 +129,49 @@ class HandCompensater(Application):
         weight[9,9] = weight_knee
         weight[15,15] = weight_knee
         weight[19,19] = weight_chesttilt
-        #weight = weight[6:,:]
+        weight = weight[6:,:]
 
-        self.featurePosture.jacobianIN.value = matrixToTuple(weight)
-        self.featurePostureDes.errorIN.value = self.robot.halfSitting
-        mask = '1'*36
-        # mask = '000000000000111100000000000000000000000000'
+        self.taskPosture.task.jacobian.value = matrixToTuple(weight)
+        self.taskPosture.task.task.value = (0,)*weight.shape[0]
+        mask = '000000000000111100000000000000'
         # robot.dynamic.displaySignals ()
         # robot.dynamic.Jchest.value
-        self.features['posture'].selec.value = mask
+        self.taskPosture.feature.selec.value = mask
 
     def initTaskGains(self, setup = "medium"):
         if setup == "medium":
-            self.gains['balance'].setConstant(10)
-            self.gains['chest'].setConstant(10)
-            self.gains['right-wrist'].setByPoint(4,0.2,0.01,0.8)
-            self.gains['left-wrist'].setByPoint(4,0.2,0.01,0.8)
+            self.contactRF.gain.setConstant(10)
+            self.contactLF.gain.setConstant(10)
+            self.taskChest.gain.setConstant(10)
+            self.taskRH.gain.setByPoint(4,0.2,0.01,0.8)
+            self.taskCompensateR.gain.setByPoint(4,0.2,0.01,0.8)
+            if self.twoHands:
+                self.taskCompensateL.gain.setByPoint(4,0.2,0.01,0.8)
+                self.taskLH.gain.setByPoint(4,0.2,0.01,0.8)
+
+            #self.taskCompensate.gain.setConstant(2)
             self.taskHalfStitting.gain.setByPoint(2,0.2,0.01,0.8)
          
     # --- SOLVER ----------------------------------------------------------------
 
-    def push(self,task,feature=None,keep=False):
+    def initSolver(self):
+        plug(self.sot.control,self.robot.device.control)
+
+    def push(self,task,keep=False):
         if isinstance(task,str): taskName=task
         elif "task" in task.__dict__:  taskName=task.task.name
         else: taskName=task.name
-        if taskName not in toList(self.sot):
+        if taskName not in self.sot.toList():
             self.sot.push(taskName)
-        if taskName!="posture" and "posture" in toList(self.sot):
-            self.sot.down("posture")
-        if keep: feature.keep()
+        if taskName!="taskposture" and "taskposture" in self.sot.toList():
+            self.sot.down("taskposture")
+        if keep: task.keep()
 
     def rm(self,task):
         if isinstance(task,str): taskName=task
         elif "task" in task.__dict__:  taskName=task.task.name
         else: taskName=task.name
-        if taskName in toList(self.sot): self.sot.remove(taskName)
+        if taskName in self.sot.toList(): self.sot.rm(taskName)
 
     # --- DISPLAY -------------------------------------------------------------
     def initDisplay(self):
@@ -162,31 +205,32 @@ class HandCompensater(Application):
     # --- RUN --------------------------------------------------------------
     def initialStack(self):
         self.sot.clear()
-        self.push(self.tasks['balance'])
-        self.push(self.taskChest,self.features['chest'],True)
+        self.push(self.contactLF,True)
+        self.push(self.contactRF,True)
+        self.push(self.taskLim)
+        self.push(self.taskCom)
+        self.push(self.taskChest,True)
         self.push(self.taskPosture)
 
     def moveToInit(self):
         '''Go to initial pose.'''
         #gotoNd(self.taskRH,(0.3,-0.2,1.1,0,-pi/2,0),'111001')
         #gotoNd(self.taskLH,(0.3,0.2,1.1,0,-pi/2,0),'111001')
-        change6dPositionReference(self.taskRH,self.features['right-wrist'],\
-                                    self.gains['right-wrist'],\
-                                    (0.3,-0.2,1.1,0,-pi/2,0),'111111')
+        gotoNd(self.taskRH,(0.3,-0.2,1.1,0,-pi/2,0),'111001')
         self.push(self.taskRH)
         if self.twoHands:
-            change6dPositionReference(self.taskLH,self.features['left-wrist'],\
-                                    self.gains['left-wrist'],\
-                                     (0.3,0.2,1.1,0,-pi/2,0),'111111')
+            gotoNd(self.taskLH,(0.3,0.2,1.1,0,-pi/2,0),'111001')
             self.push(self.taskLH)
+
         None
 
     def goHalfSitting(self):
         '''End of application, go to final pose.'''
-        self.featurePostureDes.errorIN.value = self.robot.halfSitting
         self.sot.clear()
-        self.push(self.tasks['balance'])
-        self.push(self.taskPosture)
+        self.push(self.contactLF,True)
+        self.push(self.contactRF,True)
+        self.push(self.taskLim)
+        self.push(self.taskHalfStitting)
 
     # --- SEQUENCER ---
     seqstep = 0
@@ -232,53 +276,48 @@ class HandCompensater(Application):
         self.ccMrhref = self.transformerR.gM0 # reference matrix homo in the control frame
         self.ccVrhref = self.transformerR.gV0
         
-        ######
+        plug(self.ccMrhref,self.taskCompensateR.featureDes.position)
+        plug(self.ccVrhref,self.taskCompensateR.featureDes.velocity)
+        
+        self.taskCompensateR.task.setWithDerivative (True)
+        self.taskCompensateR.feature.frame('desired')
 
+        ######
         if self.twoHands:
             self.transformerL = sotso.MovingFrameTransformation('tranformation_left')
+
+            plug(self.ccMrhref,self.taskCompensateL.featureDes.position)
+            plug(self.ccVrhref,self.taskCompensateL.featureDes.velocity)
 
             self.sym = Multiply_of_matrixHomo('sym')
 
             self.sym.sin1.value =((1, 0, 0, 0), (0, -1, 0, 0), (0, 0, 1, 0), (0, 0, 0, 1))
             plug (self.ccMrhref,self.sym.sin2)
-            
+            plug (self.sym.sout,self.taskCompensateL.featureDes.position)
 
 
             self.symVel = Multiply_matrix_vector('symvel')
             self.symVel.sin1.value =((1,0,0,0,0,0),(0,-1,0,0,0,0),(0,0,0,1,0,0),(0,0,0,1,0,0),(0,0,0,0,-1,0),(0,0,0,0,0,1))
             plug (self.ccVrhref,self.symVel.sin2)
+            plug (self.symVel.sout,self.taskCompensateL.featureDes.velocity)
+            self.taskCompensateL.feature.selec.value='000111'
             
+            self.taskCompensateL.task.setWithDerivative (True)
+            self.taskCompensateL.feature.frame('desired')
         
 
-            self.cMrhref.value = (matrixToTuple(diag([1,1,1,1])))
-            self.cVrhref.value = (0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0)
+        self.cMrhref.value = (matrixToTuple(diag([1,1,1,1])))
+        self.cVrhref.value = (0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0)
+
+       
+
 
     def startCompensate(self):
-
-
         '''Start to compensate for the hand movements.'''
-        self.cMrhref.value = self.robot.dynamic.signal('right-wrist').value
+     
+        self.cMrhref.value = self.robot.dynamic.rh.value
         self.cVrhref.value = (0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0)
-        
-        plug(self.ccMrhref,self.features['right-wrist'].reference)
-        plug(self.ccVrhref,self.features['right-wrist'].velocity)
-        
-        self.gains['right-wrist'].setByPoint(4,0.2,0.01,0.8)        
-        self.tasks['right-wrist'].setWithDerivative (True)
-        self.features['right-wrist'].frame('desired')
-        
-
-
         print matrix(self.cMrhref.value)
-
-        if self.twoHands:
-            self.gains['left-wrist'].setByPoint(4,0.2,0.01,0.8)
-            
-            plug (self.sym.sout,self.features['left-wrist'].reference)
-            plug (self.symVel.sout,self.features['left-wrist'].velocity)
-            self.features['left-wrist'].selec.value='000111'
-            self.tasks['left-wrist'].setWithDerivative (True)
-            self.features['left-wrist'].frame('desired')
                
 
         #######
@@ -288,3 +327,10 @@ class HandCompensater(Application):
         #print matrix(self.cMlhref.value)
 
         ######
+        
+        self.rm(self.taskRH)
+        self.push(self.taskCompensateR)
+
+        if self.twoHands:
+            self.rm(self.taskLH)
+            self.push(self.taskCompensateL)
