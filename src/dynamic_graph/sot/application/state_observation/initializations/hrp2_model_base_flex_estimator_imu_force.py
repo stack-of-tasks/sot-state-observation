1e99294fd9560158a74696a11c69bf91c9a73e6c
Alexis MIFSUD <alexis.mifsud@gmail.com>
2015-11-27 19:18:17 +0100

    cleaning

--- src/dynamic_graph/sot/application/state_observation/initializations/hrp2_model_base_flex_estimator_imu_force.py
+++ src/dynamic_graph/sot/application/state_observation/initializations/hrp2_model_base_flex_estimator_imu_force.py
@@ -21,7 +21,10 @@ class HRP2ModelBaseFlexEstimatorIMUForce(DGIMUModelBaseFlexEstimation):
         self.setSamplingPeriod(0.005)  
         self.robot = robot
 
-        # Definition of IMU vector
+        self.robot.dynamic.inertia.recompute(1)					      
+        self.robot.dynamic.waist.recompute(1)	
+
+        # Sensors stack
         self.sensorStackimu = Stack_of_vector (name+'SensorsIMU')
         plug(self.robot.device.accelerometer,self.sensorStackimu.sin1)
         plug(self.robot.device.gyrometer,self.sensorStackimu.sin2)
@@ -29,7 +32,7 @@ class HRP2ModelBaseFlexEstimatorIMUForce(DGIMUModelBaseFlexEstimation):
         self.sensorStackimu.selec2 (0, 3)
 	self.calibration= Calibrate('calibration')
 	plug(self.sensorStackimu.sout,self.calibration.imuIn)
-	plug(self.robot.dynamic.com,self.calibration.comIn)
+	plug(self.robot.dynamic.com,self.calibration.comIn)			
 
         self.sensorStackforce = Stack_of_vector (name+'SensorsFORCE')
         plug(self.robot.device.forceLLEG,self.sensorStackforce.sin1)
@@ -43,16 +46,19 @@ class HRP2ModelBaseFlexEstimatorIMUForce(DGIMUModelBaseFlexEstimation):
         self.sensorStack.selec1 (0, 6)
         self.sensorStack.selec2 (0, 12)
 
+	plug(self.sensorStack.sout,self.measurement);
         self.contactForces = self.sensorStack.sin2
       
-        plug(self.sensorStack.sout,self.measurement);
+        # Input reconstruction
+
+		# IMU Vector
         self.inputPos = MatrixHomoToPoseUTheta(name+'InputPosition')
         plug(robot.frames['accelerometer'].position,self.inputPos.sin)
-        self.robot.dynamic.createJacobian(name+'ChestJ_OpPoint','chest')
+        self.robot.dynamic.createJacobian(name+'ChestJ_OpPoint','chest')	
         self.imuOpPoint = OpPointModifier(name+'IMU_oppoint')
         self.imuOpPoint.setEndEffector(False)
         self.imuOpPoint.setTransformation(matrixToTuple(np.linalg.inv(np.matrix(self.robot.dynamic.chest.value))*np.matrix(self.robot.frames['accelerometer'].position.value)))
-        plug (self.robot.dynamic.chest,self.imuOpPoint.positionIN)
+        plug (self.robot.dynamic.chest,self.imuOpPoint.positionIN)			
         plug (self.robot.dynamic.signal(name+'ChestJ_OpPoint'),self.imuOpPoint.jacobianIN)
         self.inputVel = Multiply_matrix_vector(name+'InputVelocity')
         plug(self.imuOpPoint.jacobian,self.inputVel.sin1)
@@ -68,20 +74,8 @@ class HRP2ModelBaseFlexEstimatorIMUForce(DGIMUModelBaseFlexEstimation):
         self.IMUVector.outputFormat.value = '011111'
         self.IMUVector.setFiniteDifferencesInterval(2)
 
-        # Definition of inertia, angular momentum and derivatives
-        self.robot.dynamic.inertia.recompute(1)
-        self.inertia=self.robot.dynamic.inertia #(48.2378,48.2378,2.87339,0,0,0) #
-        self.dotInertia=(0,0,0,0,0,0)
-        self.zeroMomentum=(0,0,0,0,0,0)
-        
-        # Waist position
-        self.robot.dynamic.waist.recompute(1)
-        #self.robot.dynamic.chest.recompute(1)
-        #self.robot.dynamic.com.recompute(1)
-        self.positionWaist=self.robot.dynamic.waist
-
-        # Definition of com and derivatives
-        self.com=self.robot.dynamic.com#(0,0,0.75) # /!\ In the local frame!
+        	# CoM and derivatives
+        self.com=self.robot.dynamic.com
         self.DCom = Multiply_matrix_vector(name+'DCom')
         plug(self.robot.dynamic.Jcom,self.DCom.sin1)
         plug(self.robot.device.velocity,self.DCom.sin2)
@@ -93,28 +87,26 @@ class HRP2ModelBaseFlexEstimatorIMUForce(DGIMUModelBaseFlexEstimation):
         self.comVector = PositionStateReconstructor (name+'ComVector')
         plug(self.comVectorIn.sout,self.comVector.sin)
         self.comVector.inputFormat.value  = '000101'
-        self.comVector.outputFormat.value = '010101'            
-        
-        # Concatenate with InputReconstructor entity
-        self.inputVector=InputReconstructor(name+'inputVector')
-        plug(self.comVector.sout,self.inputVector.comVector)
-        plug(self.inertia,self.inputVector.inertia)
-        self.inputVector.dinertia.value=self.dotInertia
-        plug(self.positionWaist,self.inputVector.positionWaist)
-        self.inputVector.setSamplingPeriod(robot.timeStep)
-        self.inputVector.setFDInertiaDot(True)
+        self.comVector.outputFormat.value = '010101'  
 
-        plug(self.robot.dynamic.angularmomentum,self.inputVector.angMomentum)
+		# Compute derivative of Angular Momentum
         self.angMomDerivator = Derivator_of_Vector('angMomDerivator')
         plug(self.robot.dynamic.angularmomentum,self.angMomDerivator.sin)
-        self.angMomDerivator.dt.value = self.robot.timeStep
-        plug(self.angMomDerivator.sout,self.inputVector.dangMomentum)
-        #self.inputVector.dangMomentum.value = self.zeroMomentum
-
+        self.angMomDerivator.dt.value = self.robot.timeStep          
+        
+        	# Concatenate with InputReconstructor entity
+        self.inputVector=InputReconstructor(name+'inputVector')
+        plug(self.comVector.sout,self.inputVector.comVector)
+        plug(self.robot.dynamic.inertia,self.inputVector.inertia)
+	plug(self.robot.dynamic.angularmomentum,self.inputVector.angMomentum)
+	plug(self.angMomDerivator.sout,self.inputVector.dangMomentum)
+        self.inputVector.dinertia.value=(0,0,0,0,0,0)
+        plug(self.robot.dynamic.waist,self.inputVector.positionWaist)
         plug(self.IMUVector.sout,self.inputVector.imuVector)
         plug(self.contactNbr,self.inputVector.nbContacts)
-        # plug(self.contacts,self.inputVector.contactsPosition)        
-       
+
+        self.inputVector.setSamplingPeriod(robot.timeStep)
+        self.inputVector.setFDInertiaDot(True)     
         plug(self.inputVector.input,self.input)
         self.robot.flextimator = self
 
