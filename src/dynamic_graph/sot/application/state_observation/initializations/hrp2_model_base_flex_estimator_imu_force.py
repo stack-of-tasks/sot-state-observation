f26d843eaa731900e2d241fdd8d96326dce4940f
Alexis MIFSUD <alexis.mifsud@gmail.com>
2016-03-03 14:47:56 +0100

    extending state size

--- src/dynamic_graph/sot/application/state_observation/initializations/hrp2_model_base_flex_estimator_imu_force.py
+++ src/dynamic_graph/sot/application/state_observation/initializations/hrp2_model_base_flex_estimator_imu_force.py
@@ -26,7 +26,7 @@ class HRP2ModelBaseFlexEstimatorIMUForce(DGIMUModelBaseFlexEstimation):
 	self.setForceVariance(1e-4)
 	self.setWithComBias(False)
         
-	self.setProcessNoiseCovariance(matrixToTuple(np.diag((1e-8,)*12+(1e-4,)*6+(1.e-13,)*2+(1e-8,)*3)))
+	self.setProcessNoiseCovariance(matrixToTuple(np.diag((1e-8,)*12+(1e-4,)*6+(1.e-13,)*2+(1e-8,)*3+(0.e-0,)*6)))
 	self.setMeasurementNoiseCovariance(matrixToTuple(np.diag((1e-3,)*3+(1e-6,)*3)))
 
 	# Odometry
